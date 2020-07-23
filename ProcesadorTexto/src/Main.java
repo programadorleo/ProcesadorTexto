becdbfb1e05c9a8223ceff895528301fb968df5d
programadorleo <gandariasleo@gmail.com>
2020-07-23 09:50:05 -0300

    Version Inicial procesador texto

---
+++ ProcesadorTexto/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	
+	
+		public static void main(String[] arg) {
+			  
+			 // Marco marcO22= new Marco();
+			//  marcO22.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//  marcO22.setVisible(true);
+			//  
+			 }
+
+		}
+
+	
+
+
